75793d50bd52698c1dc7ad65c8442f830b952f16
Sebastian Garcia <jusebastian10@hotmail.com>
2023-03-21 01:35:47 -0500

    funtions

--- script.js
+++ script.js
@@ -3,48 +3,50 @@ import * as mathigon from '@mathigon/fermat';
 import { DarkModeToggle } from 'dark-mode-toggle';
 import * as ss from 'simple-statistics';
 
-
 document.getElementById("form").addEventListener("submit",function(event){
     event.preventDefault();
-    resultado();
-    operaciones(resultado()); 
-    });
 
+    let opcionMedia = document.getElementById("media").checked;
+    let opcionMediana = document.getElementById("mediana").checked;
+    let opcionModa = document.getElementById("moda").checked;
+    let opcionDesviacion = document.getElementById("desviacion").checked;
 
+    if(opcionMedia == true){ operacionMean(resultado());
+    } else{ document.getElementById("respuesta").innerHTML= "";}
+    if(opcionMediana == true){operacionMedian(resultado());}
+    if(opcionModa == true){operacionMode(resultado());}
+    if(opcionDesviacion == true){operacionSTD(resultado());}
+    
+});
 function resultado(){
-
     const datos = document.getElementById("ingresoDatos").value;
-    console.log(isNumber(datos));
     console.log(datos.split(","));
     let arregloDatos=datos.split(',');
     let arregloDatosConvertida=arregloDatos.map(function(item){return parseInt(item)});
     console.log(arregloDatos.length + "numero del arreglo");
 
     return arregloDatosConvertida;
-    
 }
-
 function operaciones(array){
-
-
     let min = statistical.methods.min(array);
-    let median1 = statistical.methods.median(array);
-    let mode1 = statistical.methods.mode(array);
-    let mean = ss.mean(array);
-    let variances = statistical.methods.variance(array);
-    let std = statistical.methods.stdDeviation(array);
-
-    console.log(min + "minimo");
-    console.log(median1 +"mediana");
-    console.log(mode1 +"moda");
-    console.log(mean + "media");
-    console.log(variances+ "varianza");
-    console.log(std + "desviación");
-
+    let variances = statistical.methods.variance(array); 
 }
-
-
-
+function operacionSTD(array){
+    let std = statistical.methods.stdDeviation(array);
+    document.getElementById("respuesta").innerHTML+= (  "<br> la desviación es: "+ std.toFixed(2));
+}
+function operacionMedian(array){
+    let median1 = statistical.methods.median(array);
+    document.getElementById("respuesta").innerHTML+= (  "<br> la mediana es: "+ median1.toFixed(2));
+}
+function operacionMode(array){
+    let mode1 = statistical.methods.mode(array);
+    document.getElementById("respuesta").innerHTML+= (  "<br> la moda es: "+ mode1);
+}
+function operacionMean(array){
+    let mean = ss.mean(array);
+    document.getElementById("respuesta").innerHTML= (  "la media es: "+ mean.toFixed(2));
+}
 const isNumber = n => (typeof(n) === 'number' || n instanceof Number || (typeof(n) === 'string' && !isNaN(n))) && isFinite(n);
 
 
